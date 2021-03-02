c9dddaafba768951a13df7b1a3e6d32f06a65987
adnanjamil123 <49995849+adnanjamil123@users.noreply.github.com>
2021-03-02 20:25:37 +0300

    migrations

---
+++ SparkAuto/Data/Migrations/20210302171259_QTY.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SparkAuto.Data.Migrations
+{
+    public partial class QTY : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "qty",
+                table: "ServiceType",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "qty",
+                table: "ServiceType");
+        }
+    }
+}
---
+++ SparkAuto/Data/Migrations/20210302172258_DEL.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SparkAuto.Data.Migrations
+{
+    public partial class DEL : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "qty",
+                table: "ServiceType");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "qty",
+                table: "ServiceType",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+    }
+}
